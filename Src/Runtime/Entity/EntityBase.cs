d641263d1f9c388a17e28423351c9346b4626056
mangit <1048743890@qq.com>
2022-07-13 20:32:35 +0800

    entity base

---
+++ Src/Runtime/Entity/EntityBase.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+
+/// <summary>
+/// 场景实体,前后端可以直接使用，或者根据差异化派生出新的类
+/// </summary>
+public class EntityBase
+{
+    /// <summary>
+    /// 逻辑实体根节点 可以挂载逻辑实体相关逻辑 一定不为空
+    /// </summary>
+    /// <value></value>
+    protected GameObject Root;
+
+    /// <summary>
+    /// 场景实体变换 也是Root节点的变换 一定不为空
+    /// </summary>
+    public Transform Transform => Root.transform;
+
+    /// <summary>
+    /// entity base data
+    /// </summary>
+    public EntityBaseData BaseData { get; protected set; }
+
+    public EntityBase()
+    {
+        InitRoot();
+        BaseData = Root.AddComponent<EntityBaseData>();
+    }
+
+    public virtual void Init()
+    {
+        InitToScene();
+    }
+
+    public virtual void Dispose()
+    {
+        UnInitFromScene();
+        BaseData.Reset();
+        Object.Destroy(Root);
+        Root = null;
+    }
+
+    protected virtual void InitRoot()
+    {
+        Root = new GameObject();
+    }
+
+    /// <summary>
+    /// 添加到场景中
+    /// </summary>
+    protected virtual void InitToScene()
+    {
+        //todo:
+    }
+
+    protected virtual void UnInitFromScene()
+    {
+        //todo;
+    }
+
+    public void InitBaseInfo(long id, eEntityType type)
+    {
+        BaseData.Init(id, type);
+        Root.name = $"{type}_{id}";
+    }
+
+    public void SetRootName(string name)
+    {
+        Root.name = name;
+    }
+
+    public void SetRootParent(Transform parent)
+    {
+        Transform.SetParent(parent, false);
+    }
+
+    public T GetComponent<T>() where T : Component
+    {
+        return Root.GetComponent<T>();
+    }
+
+    public T AddComponent<T>() where T : Component
+    {
+        return Root.AddComponent<T>();
+    }
+
+    public bool TryGetComponent<T>(out T component) where T : Component
+    {
+        return Root.TryGetComponent(out component);
+    }
+}
