52fd4c51bc011dbb7e29b83918e52d11d01b0b86
a412555466 <412555466@qq.com>
2022-07-19 19:31:51 +0800

    Added skill effect ball system

---
+++ Src/Runtime/Battle/BattleDefine.cs
@@ -0,0 +1,16 @@
+/*
+ * @Author: xiang huan
+ * @Date: 2022-07-19 10:51:41
+ * @Description: 战斗公共定义
+ * @FilePath: /meland-unity/Assets/Plugins/SharedCore/Src/Runtime/Battle/BattleDefine.cs
+ * 
+ */
+
+public static class BattleDefine
+{
+    public enum eSkillEffectId : int
+    {
+        EffectIdUnknown = 0,
+        NormalDamageSE = 1000,
+    }
+}
