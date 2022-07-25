6f416797e3063dca4b2e61bf3bace711a78750a2
qqoo6789 <270949894@qq.com>
2022-07-25 11:16:38 +0800

    change path

--- Src/Editor/Common/Constant.cs
+++ Src/Editor/Common/Constant.cs
@@ -13,19 +13,21 @@ namespace SharedCore.Editor
         }
 
         // 目录分割符
+        public static readonly char Spt = Path.DirectorySeparatorChar;
+        // 系统的 /bin/sh 命令
         public static readonly string BinSh = Path.Combine("/bin/sh");
         // 项目路径 meland-unity
         public static readonly string ProjectPath = System.Environment.CurrentDirectory;
         // 项目Assets资源路径 meland-unity/Assets
         public static readonly string AssetsPath = Path.Combine(ProjectPath, "Assets");
+        // SharedCore路径 meland-unity/Assets/Plugins/SharedCore
+        public static readonly string SharedCorePath = Path.Combine(AssetsPath, "Plugins/SharedCore");
         // 项目临时文件夹 meland-unity/Temp
         public static readonly string TempPath = Path.Combine(ProjectPath, "Temp");
         // 编辑器插件 meland-unity/Assets/Plugins/SharedCore/Src/Editor 
-        public static readonly string EditorPath = Path.Combine(AssetsPath, "Plugins/SharedCore/Src/Editor");
+        public static readonly string EditorPath = Path.Combine(SharedCorePath, "Src/Editor");
         // 通用的用于执行命令的sh文件 meland-unity/Assets/Plugins/SharedCore/Src/Editor/Common/CommonBash.sh
         public static readonly string CommonHandleSh = Path.Combine(EditorPath, "Common/CommonBash.sh");
-        // 系统的 /bin/sh 命令
-        public static readonly char Spt = Path.DirectorySeparatorChar;
     }
 
 }
