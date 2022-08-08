2114f4250718c65d347f40f9b0d98e70850c683a
XQ <807929448@qq.com>
2022-08-08 20:51:51 +0800

    pr comment

--- Src/Runtime/Define/ErrorCode.cs
+++ Src/Runtime/Define/ErrorCode.cs
@@ -1,3 +1,8 @@
+/** 
+ * @Author XQ
+ * @Date 2022-08-05 12:54:47
+ * @FilePath /Assets/Plugins/SharedCore/Src/Runtime/Define/ErrorCode.cs
+ */
 /// <summary>
 /// 统一errorCode 首位代表常规错误码还是s2s 前3位模块 后3位模块内 预留头2位将来用
 /// </summary>
@@ -6,10 +11,10 @@ public enum eErrorCode : int
     success = 0,
 
     #region  常规错误码 主要是S2C 但是有些通用
-    SelfMoveFail = 0001001,
+    selfMoveFail = 0001001,
     #endregion
 
     #region  S2S错误码
-    NotFindUserData = 1001404
+    notFindUserData = 1001404
     #endregion
 }
