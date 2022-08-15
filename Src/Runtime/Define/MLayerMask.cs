ad21a280049aa27242db4610b38e86bb693b439f
a412555466 <412555466@qq.com>
2022-08-15 10:46:55 +0800

    layermask change

--- Src/Runtime/Define/MLayerMask.cs
+++ Src/Runtime/Define/MLayerMask.cs
@@ -2,11 +2,11 @@
  * @Author: xiang huan
  * @Date: 2022-07-19 10:51:41
  * @Description: layer层级
- * @FilePath: /meland-scene-server/Assets/Plugins/SharedCore/Src/Runtime/Define/LayerMask.cs
+ * @FilePath: /meland-unity/Assets/Plugins/SharedCore/Src/Runtime/Define/LayerMask.cs
  * 
  */
 
-public static class LayerMask
+public static class MLayerMask
 {
     public const int MONSTER = 6;
     public const int PLAYER = 7;
