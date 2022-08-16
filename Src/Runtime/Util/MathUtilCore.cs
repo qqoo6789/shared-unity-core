98e5a9fbf2c8e755f1f74a3957ccd1f6e57a6fe1
a412555466 <412555466@qq.com>
2022-08-16 10:16:50 +0800

    Move repeat Math UTIl

--- Src/Runtime/Util/MathUtilCore.cs
+++ Src/Runtime/Util/MathUtilCore.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.AI;
 
 /// <summary>
 /// 自定义的一些数学工具
@@ -46,5 +47,17 @@ public static class MathUtilCore
 
         float num = (plane - ray.origin.y) / dir.y;
         return ray.origin + (ray.direction * num);
+    }
+
+    /// <summary>
+    /// 根据给定的位置，获取离位置最近的课可行走位置，如果当前position可行走，则直接返回position
+    /// </summary>
+    /// <param name="position"></param>
+    /// <param name="maxError"></param>
+    /// <returns></returns>
+    public static Vector3 SampleTerrainWalkablePos(Vector3 position, float maxError = 10f)
+    {
+        _ = NavMesh.SamplePosition(position, out NavMeshHit hit, maxError, NavMesh.AllAreas);
+        return hit.position;
     }
 }
