c083327f26f532853fd58d1c0d8896a0e949038b
mangit <1048743890@qq.com>
2022-08-22 17:42:00 +0800

    use AddModule to add module

--- Src/Runtime/Utility/SharedGFEntryUtil.cs
+++ Src/Runtime/Utility/SharedGFEntryUtil.cs
@@ -2,7 +2,7 @@
  * @Author: xiang huan
  * @Date: 2022-07-26 15:53:11
  * @Description: 共享库GFEntry设置工具
- * @FilePath: /meland-scene-server/Assets/Plugins/SharedCore/Src/Runtime/Utility/SharedGFEntryUtil.cs
+ * @FilePath: /Assets/Plugins/SharedCore/Src/Runtime/Utility/SharedGFEntryUtil.cs
  * 
  */
 
@@ -14,10 +14,10 @@ public static class SharedGFEntryUtil
 {
     public static void SetDataTable(IDataTableComponent dataTable)
     {
-        GFEntryCore.SetDataTable(dataTable);
+        GFEntryCore.AddModule(dataTable);
     }
     public static void SetSkillEffectFactory(SkillEffectCoreFactory skillEffectFactory)
     {
-        GFEntryCore.SetSkillEffectFactory(skillEffectFactory);
+        GFEntryCore.AddModule(skillEffectFactory);
     }
 }
