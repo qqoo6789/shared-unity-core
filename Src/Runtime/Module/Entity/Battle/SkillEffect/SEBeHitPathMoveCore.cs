ec6dc514dbca04ab4a6c7c71db88a52f499b528a
a412555466 <412555466@qq.com>
2022-09-06 16:23:50 +0800

    change error

--- Src/Runtime/Module/Entity/Battle/SkillEffect/SEBeHitPathMoveCore.cs
+++ Src/Runtime/Module/Entity/Battle/SkillEffect/SEBeHitPathMoveCore.cs
@@ -2,7 +2,7 @@
  * @Author: xiang huan
  * @Date: 2022-08-12 14:36:36
  * @Description: 受击路径移动效果
- * @FilePath: /meland-unity/Assets/Plugins/SharedCore/Src/Runtime/Module/Entity/Battle/SkillEffect/SEBeHitPathMoveCore.cs
+ * @FilePath: /meland-scene-server/Assets/Plugins/SharedCore/Src/Runtime/Module/Entity/Battle/SkillEffect/SEBeHitPathMoveCore.cs
  * 
  */
 
@@ -33,7 +33,7 @@ public class SEBeHitPathMoveCore : SEPathMoveCore
                 return false;
             }
             //霸体效果
-            if (!targetBattleData.HasBattleEffect(BattleDefine.eBattleEffectKey.Endure))
+            if (targetBattleData.HasBattleEffect(BattleDefine.eBattleEffectKey.Endure))
             {
                 return false;
             }
