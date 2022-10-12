e2861809e2499eb217c65f9453397475fa5b387b
XQ <807929448@qq.com>
2022-10-12 16:11:05 +0800

    add move deine

--- Src/Runtime/Define/MoveDefine.cs
+++ Src/Runtime/Define/MoveDefine.cs
@@ -4,6 +4,14 @@
 public static class MoveDefine
 {
     /// <summary>
+    /// 走直线时的最大请求间隔时间 s
+    /// </summary>
+    public const float MOVE_MAX_SYNC_INTERVAL_TIME = 0.5f;
+    /// <summary>
+    /// 允许的网络延迟时间 s
+    /// </summary>
+    public const float MOVE_ALLOW_NETWORK_DELAY_TIME = 0.1f;
+    /// <summary>
     /// 移动的步高 也就是能移动的直线高度
     /// </summary>
     public const float MOVE_STEP_HEIGHT = 0.3f;
