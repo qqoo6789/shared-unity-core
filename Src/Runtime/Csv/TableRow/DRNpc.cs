aa84104a0879bb7091b70a63c7e4eee3f3ed0b35
a412555466 <412555466@qq.com>
2022-10-16 15:57:14 +0800

    add npc config

---
+++ Src/Runtime/Csv/TableRow/DRNpc.cs
@@ -0,0 +1,105 @@
+﻿//------------------------------------------------------------
+// 此文件由工具自动生成
+//------------------------------------------------------------
+
+using GameFramework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityGameFramework.Runtime;
+
+
+/// <summary>
+/** __DATA_TABLE_COMMENT__*/
+/// </summary>
+public class DRNpc : DataRowBase
+{
+    private int _id = 0;
+
+    /// <summary>
+    /// /**获取id。*/
+    /// </summary>
+    public override int Id => _id;
+
+    /// <summary>
+  /**获取名字。*/
+    /// </summary>
+    public string Name
+    {
+        get;
+        private set;
+    }
+
+    /// <summary>
+  /**获取Npc的描述。*/
+    /// </summary>
+    public string Desc
+    {
+        get;
+        private set;
+    }
+
+    /// <summary>
+  /**获取Npc的Icon1。*/
+    /// </summary>
+    public string Icon
+    {
+        get;
+        private set;
+    }
+
+    /// <summary>
+  /**获取RoleAssetID。*/
+    /// </summary>
+    public int RoleAssetID
+    {
+        get;
+        private set;
+    }
+
+    /// <summary>
+  /**获取移动速度。*/
+    /// </summary>
+    public int MoveSpeed
+    {
+        get;
+        private set;
+    }
+
+    public override bool ParseDataRow(string dataRowString, object userData)
+    {
+        string[] columnStrings = CSVSerializer.ParseCSVCol(dataRowString);
+
+        int index = 0;
+        _id = int.Parse(columnStrings[index++]);
+        Name = columnStrings[index++];
+        Desc = columnStrings[index++];
+        Icon = columnStrings[index++];
+        RoleAssetID = DataTableParseUtil.ParseInt(columnStrings[index++]);
+        MoveSpeed = DataTableParseUtil.ParseInt(columnStrings[index++]);
+
+        return true;
+    }
+
+
+    public override bool ParseDataRow(byte[] dataRowBytes, int startIndex, int length, object userData)
+    {
+        using (MemoryStream memoryStream = new(dataRowBytes, startIndex, length, false))
+        {
+            using (BinaryReader binaryReader = new(memoryStream, Encoding.UTF8))
+            {
+                _id = binaryReader.Read7BitEncodedInt32();
+                Name = binaryReader.ReadString();
+                Desc = binaryReader.ReadString();
+                Icon = binaryReader.ReadString();
+                RoleAssetID = binaryReader.Read7BitEncodedInt32();
+                MoveSpeed = binaryReader.Read7BitEncodedInt32();
+            }
+        }
+
+        return true;
+    }
+}
+
