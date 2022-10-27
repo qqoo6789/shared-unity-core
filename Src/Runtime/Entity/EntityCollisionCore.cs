9d3856a9c942c6d50cf25e724911cce8ffe2cd28
qqoo6789 <270949894@qq.com>
2022-10-27 20:30:53 +0800

    static entity collision

--- Src/Runtime/Entity/EntityCollisionCore.cs
+++ Src/Runtime/Entity/EntityCollisionCore.cs
@@ -2,7 +2,7 @@
  * @Author: xiang huan
  * @Date: 2022-08-26 14:25:46
  * @Description: 实体碰撞盒
- * @FilePath: /meland-unity/Assets/Plugins/SharedCore/Src/Runtime/Entity/EntityCollisionCore.cs
+ * @FilePath: /Assets/Plugins/SharedCore/Src/Runtime/Entity/EntityCollisionCore.cs
  * 
  */
 using CMF;
@@ -62,30 +62,39 @@ public abstract class EntityCollisionCore : EntityBaseComponent
         }
 
         //因为现在移动主要都是依赖角色控制器 但是角色控制器控制的移动只能是控制器所在对象 所以只能将预制件中的碰撞器参数复制到实体根上来移动 这是暂时折中办法
-        if (!prefab.TryGetComponent(out Mover prefabMover))
+        if (prefab.TryGetComponent(out Mover prefabMover))
         {
-            Log.Error($"not find CharacterMovement of collider prefab name:{RefEntity.BaseData.Id}");
-            return;
+            //先手动创建移动碰撞的依赖组件
+            _ = RefEntity.AddComponent<Rigidbody>();
+            CapsuleCollider collider = RefEntity.AddComponent<CapsuleCollider>();
+
+            Mover mover = RefEntity.AddComponent<Mover>();
+            mover.SetColliderHeight(prefabMover.ColliderHeight);
+            mover.SetColliderThickness(prefabMover.ColliderThickness);
+            mover.SetColliderOffset(prefabMover.ColliderOffset);
+            mover.SetStepHeightRatio(MoveDefine.MOVE_STEP_HEIGHT_RATIO);
+            // characterMovement.center = prefabCharacterMovement.center;
+            // characterMovement.slopeLimit = MoveDefine.MOVE_SLOPE_LIMIT;
+            // characterMovement.stepOffset = MoveDefine.MOVE_STEP_HEIGHT;
+            // characterMovement.collisionLayers = MLayerMask.MASK_SCENE_OBSTRUCTION;
+            // characterMovement.skinWidth = MoveDefine.MOVE_SKIN_WIDTH;
+            RefEntity.EntityRoot.layer = prefab.layer;//暂时直接直接赋值成碰撞盒的层
+
+            CollisionObject = collider.gameObject;
+            BodyCollision = collider;
+            // Log.Error($"not find CharacterMovement of collider prefab name:{RefEntity.BaseData.Id}");
+        }
+        else
+        {
+            // 静态实体
+            CollisionObject = GameObject.Instantiate(prefab);
+            CollisionObject.transform.parent = RefEntity.GetTransform();
+            CollisionObject.transform.localPosition = Vector3.zero;
+            CollisionObject.transform.localRotation = Quaternion.identity;
+            BodyCollision = CollisionObject.GetComponent<Collider>();
         }
 
-        //先手动创建移动碰撞的依赖组件
-        _ = RefEntity.AddComponent<Rigidbody>();
-        CapsuleCollider collider = RefEntity.AddComponent<CapsuleCollider>();
 
-        Mover mover = RefEntity.AddComponent<Mover>();
-        mover.SetColliderHeight(prefabMover.ColliderHeight);
-        mover.SetColliderThickness(prefabMover.ColliderThickness);
-        mover.SetColliderOffset(prefabMover.ColliderOffset);
-        mover.SetStepHeightRatio(MoveDefine.MOVE_STEP_HEIGHT_RATIO);
-        // characterMovement.center = prefabCharacterMovement.center;
-        // characterMovement.slopeLimit = MoveDefine.MOVE_SLOPE_LIMIT;
-        // characterMovement.stepOffset = MoveDefine.MOVE_STEP_HEIGHT;
-        // characterMovement.collisionLayers = MLayerMask.MASK_SCENE_OBSTRUCTION;
-        // characterMovement.skinWidth = MoveDefine.MOVE_SKIN_WIDTH;
-        RefEntity.EntityRoot.layer = prefab.layer;//暂时直接直接赋值成碰撞盒的层
-
-        CollisionObject = collider.gameObject;
-        BodyCollision = collider;
 
         EntityReferenceData refData = RefEntity.AddComponent<EntityReferenceData>();
         refData.SetEntity(RefEntity);
