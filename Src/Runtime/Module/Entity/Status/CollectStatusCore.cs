1a7192c69d31ee4977d7ae5e29aa0e3d74b8b521
qqoo6789 <270949894@qq.com>
2022-10-31 15:49:26 +0800

    collect status

---
+++ Src/Runtime/Module/Entity/Status/CollectStatusCore.cs
@@ -0,0 +1,64 @@
+
+using System;
+using GameFramework.Fsm;
+
+/// <summary>
+/// 采集状态通用状态基类
+/// </summary>
+public class CollectStatusCore : ListenEventStatusCore, IEntityCanMove, IEntityCanSkill
+{
+    public static new string Name = "Collect";
+
+    public override string StatusName => Name;
+
+    private EntityInputData _inputData;
+
+    protected override Type[] EventFunctionTypes => new Type[] {
+        typeof(BeHitMoveEventFunc),
+        typeof(WaitToBattleStatusEventFunc)
+    };
+    protected override void OnEnter(IFsm<EntityStatusCtrl> fsm)
+    {
+        base.OnEnter(fsm);
+
+        _inputData = StatusCtrl.GetComponent<EntityInputData>();
+    }
+
+    protected override void OnLeave(IFsm<EntityStatusCtrl> fsm, bool isShutdown)
+    {
+        _inputData = null;
+
+        base.OnLeave(fsm, isShutdown);
+    }
+
+    protected override void OnUpdate(IFsm<EntityStatusCtrl> fsm, float elapseSeconds, float realElapseSeconds)
+    {
+        base.OnUpdate(fsm, elapseSeconds, realElapseSeconds);
+        if (StatusCtrl.RefEntity.BattleDataCore != null && !StatusCtrl.RefEntity.BattleDataCore.IsLive())
+        {
+            ChangeState(fsm, DeathStatusCore.Name);
+            return;
+        }
+        if (CheckCanMove())
+        {
+            if (_inputData.InputMoveDirection != null)
+            {
+                ChangeState(fsm, DirectionMoveStatusCore.Name);
+            }
+            else if (_inputData.InputMovePath.Count > 0)
+            {
+                ChangeState(fsm, PathMoveStatusCore.Name);
+            }
+        }
+    }
+
+    public bool CheckCanMove()
+    {
+        return true;
+    }
+
+    public bool CheckCanSkill(int skillId)
+    {
+        return true;
+    }
+}
