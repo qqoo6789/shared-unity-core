4f1dff4d26689799d8b3af5c32026af42e443d8e
qqoo6789 <270949894@qq.com>
2022-11-17 14:04:24 +0800

    ref and remove event

--- Src/Runtime/Module/Entity/EntityEvent.cs
+++ Src/Runtime/Module/Entity/EntityEvent.cs
@@ -20,12 +20,6 @@ public class EntityEvent : EntityBaseComponent
     /// </summary>
     public Action EntityBeReborn;
 
-
-    /// <summary>
-    /// 实体开始采集
-    /// </summary>
-    public Action EntityCollect;
-
     /// <summary>
     /// 碰撞盒加载完成 T0：碰撞盒所在GameObject
     /// </summary>
