36f9b80e71dc8565b73b6a73fcde1114f6b7c03b
XQ <807929448@qq.com>
2022-12-01 15:25:31 +0800

    home soil manager
    (cherry picked from commit 509b25662079089feb348da7cbe446449729f253)

---
+++ Src/Runtime/Module/Home/HomeDefine.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+/// <summary>
+/// 家园系统的定义
+/// </summary>
+public static class HomeDefine
+{
+    //TODO: 需要正式配置
+    public const int SOIL_X_NUM = 10;
+    public const int SOIL_Z_NUM = 10;
+    public static readonly Vector3 SOIL_SIZE = Vector3.one;
+}
