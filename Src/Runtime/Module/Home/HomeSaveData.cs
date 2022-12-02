fb7993860c5750272cf1fdf7e519401ce7b88ad2
XQ <807929448@qq.com>
2022-12-02 13:48:51 +0800

    save data
    (cherry picked from commit 53ccdcc3d3e0c3d45587ecee2653803975e5e390)

--- Src/Runtime/Module/Home/HomeSaveData.cs
+++ Src/Runtime/Module/Home/HomeSaveData.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 /// <summary>
 /// 整个家园的存储数据 可能是数据库来的 也可能是给客户端协议里解来的
@@ -11,4 +12,9 @@ public class HomeSaveData
     /// 所有非空白idle状态的土地列表 空白的不存储
     /// </summary>
     public List<SoilSaveData> SoilDataList;
+
+    public string ToJson()
+    {
+        return JsonUtility.ToJson(this);
+    }
 }
