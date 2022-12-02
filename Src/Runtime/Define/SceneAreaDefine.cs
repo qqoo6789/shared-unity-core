c11fe9986424ea80420b9aa089d6cee32c9ce636
a412555466 <412555466@qq.com>
2022-12-02 15:30:03 +0800

    add area manage

---
+++ Src/Runtime/Define/SceneAreaDefine.cs
@@ -0,0 +1,17 @@
+/*
+ * @Author: xiang huan
+ * @Date: 2022-12-02 13:54:41
+ * @Description: 区域定义
+ * @FilePath: /meland-unity/Assets/Plugins/SharedCore/Src/Runtime/Define/SceneAreaDefine.cs
+ * 
+ */
+
+
+/// <summary>
+/// 场景区域定义
+/// </summary>
+public enum eSceneArea
+{
+    world,  //大世界
+    home,   //家园
+}
