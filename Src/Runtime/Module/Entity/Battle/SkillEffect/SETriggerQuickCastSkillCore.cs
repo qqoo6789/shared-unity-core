83837f465e070e2279147cca26e327c5a756998d
a412555466 <412555466@qq.com>
2023-02-08 16:37:54 +0800

    quick cast change

--- Src/Runtime/Module/Entity/Battle/SkillEffect/SETriggerQuickCastSkillCore.cs
+++ Src/Runtime/Module/Entity/Battle/SkillEffect/SETriggerQuickCastSkillCore.cs
@@ -15,23 +15,7 @@ public class SETriggerQuickCastSkillCore : SkillEffectBase
     protected int TriggerType;
     protected int TriggerRate;
     protected int CastSkillID;
-    /// <summary>
-    /// 检测能否应用效果
-    /// </summary>
-    /// <param name="fromEntity">发送方</param>
-    /// <param name="targetEntity">接受方</param>
-    public override bool CheckApplyEffect(EntityBase fromEntity, EntityBase targetEntity)
-    {
-        //目标方已经死亡
-        if (targetEntity.BattleDataCore != null)
-        {
-            if (!targetEntity.BattleDataCore.IsLive())
-            {
-                return false;
-            }
-        }
-        return true;
-    }
+
     public override void Start()
     {
         base.Start();
