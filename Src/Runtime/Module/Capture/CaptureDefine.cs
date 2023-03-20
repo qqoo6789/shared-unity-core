85e67fbe3c39b189c9cd1cf4b6bc6ad3e6086aa1
qqoo6789 <270949894@qq.com>
2023-03-20 21:02:20 +0800

    define

--- Src/Runtime/Module/Capture/CaptureDefine.cs
+++ Src/Runtime/Module/Capture/CaptureDefine.cs
@@ -8,4 +8,6 @@ public static class CaptureDefine
     public static int MAX_CAPTURE_ROPE_HP = 3;          // 最大绳子Hp
     public static int PER_CAPTURE_ROPE_DAMAGE = -1;     // 单次对捕捉绳的伤害
     public static int MIN_CAPTURE_ROPE_LEN = 2;         // 最短捕获绳长度
+    public static int CAPTURE_ITEM_LEVEL_FACTOR = 1 / 10;   // 捕获道具等级因子
+
 }
