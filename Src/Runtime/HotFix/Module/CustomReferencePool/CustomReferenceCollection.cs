dcf5bd7a508eeb3722b7f9972d7be437ea2223ff
a412555466 <412555466@qq.com>
2023-04-26 11:06:00 +0800

    add custom reference pool

---
+++ Src/Runtime/HotFix/Module/CustomReferencePool/CustomReferenceCollection.cs
@@ -0,0 +1,142 @@
+﻿/*
+ * @Author: xiang huan
+ * @Date: 2023-04-23 20:38:52
+ * @Description: 自定义引用集合
+ * @FilePath: /meland-scene-server/Assets/Plugins/SharedCore/Src/Runtime/HotFix/Module/CustomReferencePool/CustomReferenceCollection.cs
+ * 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using GameFramework;
+using UnityGameFramework.Runtime;
+
+public class CustomReferenceCollection
+{
+    private readonly Queue<object> _references;
+    public Type ReferenceType { get; private set; }
+    public int UsingReferenceCount { get; private set; }
+    public int AcquireReferenceCount { get; private set; }
+    public int ReleaseReferenceCount { get; private set; }
+    public int AddReferenceCount { get; private set; }
+    public int RemoveReferenceCount { get; private set; }
+
+    public int UnusedReferenceCount => _references.Count;
+
+    public CustomReferenceCollection(Type referenceType)
+    {
+        _references = new Queue<object>();
+        ReferenceType = referenceType;
+        UsingReferenceCount = 0;
+        AcquireReferenceCount = 0;
+        ReleaseReferenceCount = 0;
+        AddReferenceCount = 0;
+        RemoveReferenceCount = 0;
+    }
+
+    public T Acquire<T>() where T : class, new()
+    {
+        if (typeof(T) != ReferenceType)
+        {
+            Log.Error("Custom Type is invalid.");
+            return null;
+        }
+
+        UsingReferenceCount++;
+        AcquireReferenceCount++;
+        lock (_references)
+        {
+            if (_references.Count > 0)
+            {
+                return (T)_references.Dequeue();
+            }
+        }
+        AddReferenceCount++;
+        return new T();
+    }
+    public object Acquire()
+    {
+        UsingReferenceCount++;
+        AcquireReferenceCount++;
+        lock (_references)
+        {
+            if (_references.Count > 0)
+            {
+                return _references.Dequeue();
+            }
+        }
+        AddReferenceCount++;
+        return Activator.CreateInstance(ReferenceType);
+    }
+
+    public void Release(object reference)
+    {
+        if (reference is ICustomReference customReference)
+        {
+            customReference.Clear();
+        }
+        lock (_references)
+        {
+            _references.Enqueue(reference);
+        }
+        ReleaseReferenceCount++;
+        UsingReferenceCount--;
+    }
+
+    public void Add<T>(int count) where T : class, IReference, new()
+    {
+        if (typeof(T) != ReferenceType)
+        {
+            Log.Error("Custom Type is invalid.");
+            return;
+        }
+
+        lock (_references)
+        {
+            AddReferenceCount += count;
+            while (count-- > 0)
+            {
+                _references.Enqueue(new T());
+            }
+        }
+    }
+
+    public void Add(int count)
+    {
+        lock (_references)
+        {
+            AddReferenceCount += count;
+            while (count-- > 0)
+            {
+                _references.Enqueue((IReference)Activator.CreateInstance(ReferenceType));
+            }
+        }
+    }
+
+    public void Remove(int count)
+    {
+        lock (_references)
+        {
+            if (count > _references.Count)
+            {
+                count = _references.Count;
+            }
+
+            RemoveReferenceCount += count;
+            while (count-- > 0)
+            {
+                _ = _references.Dequeue();
+            }
+        }
+    }
+
+    public void RemoveAll()
+    {
+        lock (_references)
+        {
+            RemoveReferenceCount += _references.Count;
+            _references.Clear();
+        }
+    }
+}
---
+++ Src/Runtime/HotFix/Module/CustomReferencePool/CustomReferencePool.cs
@@ -0,0 +1,108 @@
+﻿/*
+ * @Author: xiang huan
+ * @Date: 2023-04-23 19:40:38
+ * @Description: 自定义引用池, 回收时，需要自己清理引用
+ * @FilePath: /meland-scene-server/Assets/Plugins/SharedCore/Src/Runtime/HotFix/Module/CustomReferencePool/CustomReferencePool.cs
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using GameFramework;
+using UnityGameFramework.Runtime;
+
+public static class CustomReferencePool
+{
+    public static readonly Dictionary<Type, CustomReferenceCollection> ReferenceCollections = new();
+
+    /// <summary>
+    /// 清除所有引用池。
+    /// </summary>
+    public static void ClearAll()
+    {
+        lock (ReferenceCollections)
+        {
+            foreach (KeyValuePair<Type, CustomReferenceCollection> referenceCollection in ReferenceCollections)
+            {
+                referenceCollection.Value.RemoveAll();
+            }
+            ReferenceCollections.Clear();
+        }
+    }
+
+    /// <summary>
+    /// 从引用池获取引用。
+    /// </summary>
+    /// <typeparam name="T">引用类型。</typeparam>
+    /// <returns>引用。</returns>
+    public static T Acquire<T>() where T : class, new()
+    {
+
+        return GetReferenceCollection(typeof(T)).Acquire<T>();
+    }
+
+    /// <summary>
+    /// 从引用池获取引用。
+    /// </summary>
+    /// <param name="referenceType">引用类型。</param>
+    /// <returns>引用。</returns>
+    public static object Acquire(Type referenceType)
+    {
+        return GetReferenceCollection(referenceType);
+    }
+
+    /// <summary>
+    /// 将引用归还引用池。
+    /// </summary>
+    /// <param name="reference">引用。</param>
+    public static void Release(object reference)
+    {
+        if (reference == null)
+        {
+            Log.Error("Custom Reference is invalid.");
+            return;
+        }
+
+        Type referenceType = reference.GetType();
+        GetReferenceCollection(referenceType).Release(reference);
+    }
+
+    /// <summary>
+    /// 从引用池中移除所有的引用。
+    /// </summary>
+    /// <typeparam name="T">引用类型。</typeparam>
+    public static void RemoveAll<T>() where T : class, IReference
+    {
+        GetReferenceCollection(typeof(T)).RemoveAll();
+    }
+
+    /// <summary>
+    /// 从引用池中移除所有的引用。
+    /// </summary>
+    /// <param name="referenceType">引用类型。</param>
+    public static void RemoveAll(Type referenceType)
+    {
+        GetReferenceCollection(referenceType).RemoveAll();
+    }
+
+    private static CustomReferenceCollection GetReferenceCollection(Type referenceType)
+    {
+        if (referenceType == null)
+        {
+            Log.Error("Custom ReferenceType is invalid.");
+            return null;
+        }
+
+        CustomReferenceCollection referenceCollection = null;
+        lock (ReferenceCollections)
+        {
+            if (!ReferenceCollections.TryGetValue(referenceType, out referenceCollection))
+            {
+                referenceCollection = new CustomReferenceCollection(referenceType);
+                ReferenceCollections.Add(referenceType, referenceCollection);
+            }
+        }
+        return referenceCollection;
+    }
+}
+
