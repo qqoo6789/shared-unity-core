8619a6eba7c879c1dc20cd7522506d7d5429d899
a412555466 <412555466@qq.com>
2023-07-05 20:25:06 +0800

    csv change

--- Src/Runtime/Module/Entity/Battle/Skill/SkillDefine.cs
+++ Src/Runtime/Module/Entity/Battle/Skill/SkillDefine.cs
@@ -4,9 +4,9 @@ public class SkillDefine
 
 public enum eSkillFlyerType : int
 {
-    Follow = 1,       //锁定子弹
+    Follow = 0,       //锁定子弹
     Rope = 2,         //绳子子弹
     FollowRope = 3,   //锁定绳子子弹
     Quick = 4,        //快速子弹
-    Parabola = 0,     //抛物线子弹
+    Parabola = 5,     //抛物线子弹
 }
